3cd5bfb00966a8e67546fae554c529c3292d24de
destiny212121 <94549573+destiny212121@users.noreply.github.com>
2026-07-01 16:03:00 +0800

    Deploy stage-1 demo build to GitHub Pages

--- src/main.js
+++
@@ -1,5 +0,0 @@
-import Phaser from 'phaser';
-import { gameConfig } from './config/gameConfig.js';
-import GameScene from './scenes/GameScene.js';
-
-new Phaser.Game({ ...gameConfig, scene: [GameScene] });
